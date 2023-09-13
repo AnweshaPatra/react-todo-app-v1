4a9a5e81f704c3ec1d63ebd419100795330dd3ca
Anwesha <36663339+AnweshaPatra@users.noreply.github.com>
2023-09-13 15:26:17 +0530

    commit

--- react-todo-app-v1/src/App.js
+++ react-todo-app-v1/src/App.js
@@ -3,11 +3,17 @@ import TodoList from './components/TodoList';
 
 function App() {
   return (
+    <div>
     <div className="todo-app">
       <TodoList/>
-
-      
     </div>
+    <div className="about">
+    <a href="https://github.com/AnweshaPatra">
+    <img src="https://github.com/fluidicon.png" alt="GitHub Logo" />
+  </a>
+
+    </div>
+  </div>
   );
 }
 
